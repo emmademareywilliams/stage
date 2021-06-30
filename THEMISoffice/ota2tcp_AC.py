9e0d5331c51554fda9617322c044bcf23c64110c
Alexandre CUER <alexandre.cuer@cerema.fr>
2021-06-30 09:10:42 +0200

    Update ota2tcp_AC.py

--- THEMISoffice/ota2tcp_AC.py
+++ THEMISoffice/ota2tcp_AC.py
@@ -74,9 +74,9 @@ def decodeFrame(frame, length):
 
 
 class DataFlow:
-    def __init__(self, conf):
+    def __init__(self, confname):
         self._exit = False
-        self._conf = conf
+        self._confname = confname
         self._log = logging.getLogger("enless")
         self._log.setLevel("DEBUG")
         self._log.debug("........OUVERTURE DU FIELD MANAGER........")
@@ -104,12 +104,9 @@ class DataFlow:
                     if l>= 21 and frame[16:18] == bytearray([0x0f, 0x7f]):
                         if frame[18] == 0x10:
                             self._log.debug("requête d'installation en provenance de {}".format(sensorNbstr))
-                            interval = 5
-                            if self._conf:
-                                if "numbers" in self._conf:
-                                    interval = conf["interval"]
+                            interval = self._conf["interval"]
                             if frame[19] == 0x24:
-                                    interval = 3 * interval
+                                interval = 3 * interval
                             self.install(frame, interval)
                         
                         if frame[18] == 0x12:
@@ -122,16 +119,23 @@ class DataFlow:
                         
                         if frame[18] == 0x16:
                             self._log.debug('INSTALLATION EFFECTUÉE AVEC SUCCÈS')
+                            #injection du numéro de capteur dans le fichier conf
+                            # à ce stade, le fichier conf existe forcément
+                            with open(self._confname, "r+") as f:
+                                conf = json.load(f)
+                                if "numbers" not in conf:
+                                    conf["numbers"] = []
+                                if sensorNbstr not in conf["numbers"]:
+                                    conf["numbers"].append(sensorNbstr)
+                                f.seek(0)
+                                json.dump(conf, f, indent=4)
                         
                         if frame[18] in sensorTypes :
-                            # on évalue si on doit traiter les données
-                            publish = False
-                            if self._conf:
-                                if "numbers" in self._conf:
-                                    if sensorNbstr in self._conf["numbers"]:
-                                        publish=True
-                                else:
-                                    publish=True
+                            # doit-on traiter les données ?
+                            publish = True
+                            if "numbers" in self._conf:
+                                if sensorNbstr not in self._conf["numbers"]:
+                                    publish=False
                             
                             # on décode et on publie les infos du capteur dans Emoncms :        
                             if publish:
@@ -146,10 +150,12 @@ class DataFlow:
                                 else:
                                     self._log.error(msg["text"])
 
-    def writePort(self, message):
-        # écrit un message sur le port série du router
+    def write(self, message):
+        """
+        écrit un message sur la socket qui a été utilisée en lecture
+        l'appel à write doit être réalisé au sein du with de run() 
+        """
         try:
-            #self.s.close()  # on 'nettoie' la socket pour laisser la place à de nouvelles données
             n = self.s.send(message)
         except IOError as e:
             self._log.error('ERROR {}'.format(e))
@@ -162,24 +168,44 @@ class DataFlow:
         message = bytearray([length, *root, *data]) #on reconstruit le message envoyé au récepteur
         self._log.debug("Envoi du paquet d'installation")
         self._log.debug(binascii.b2a_hex(message))
-        self.writePort(message)
+        self.write(message)
 
     def RSSIresponse(self, frame, nb=0x01):
         data = bytearray([0x15, frame[19], nb, *frame[3:7][::-1], frame[-1]])
         length = len(root) + len(data)
         message = bytearray([length, *root, *data])
         self._log.debug("Envoi de la réponse RSSI {:02x}".format(nb))
-        self.writePort(message)
+        self.write(message)
 
 
-    def run(self, url):
+    def run(self, url, port):
         signal.signal(signal.SIGINT, self._sigint_handler)
         signal.signal(signal.SIGTERM, self._sigint_handler)
+        
+        # conf par défault
+        if not self._exit:
+            self._conf = {"interval":5}
+            
         while not self._exit:
+            
+            # prise en compte des modifs de configuration
+            # si le fichier conf n'existe pas, on sort du programme
+            try:
+                with open(self._confname) as f:
+                    conf =  json.loads(f.read())
+                    if "interval" not in conf:
+                        conf["interval"] = 5
+                    if conf != self._conf:
+                        self._conf = conf
+            except Exception as e:
+                self._log.debug(e)
+                self._exit = True
+                break
+
             start = None
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as self.s:
                 self.s.connect((url, port))
-                start = self.s.recv(1)  # socket.recv = équivalent de serial.read
+                start = self.s.recv(1)
                 if start and start[0] == 0x68:
                     #self._log.debug("MBUS flag detected")
                     length = self.s.recv(1)[0]
@@ -187,10 +213,10 @@ class DataFlow:
                     stop = self.s.recv(1)[0]
                     if stop == 0x16:
                         self._log.debug("0x16 détecté - paquet complet")
-                if frame:
-                    self._log.debug(binascii.b2a_hex(frame))
-                    self.analysePaquet(frame, length)
-                    self._log.debug("******************")
+                    if frame:
+                        self._log.debug(binascii.b2a_hex(frame))
+                        self.analysePaquet(frame, length)
+                        self._log.debug("******************")
             time.sleep(0.1)
 
 
@@ -202,8 +228,6 @@ class DataFlow:
     def close(self):
         #fermeture
         self._log.debug("fermeture")
-        self.s.close()
-        self._log.debug("port série fermé")
 
 
 if __name__ == "__main__":
@@ -217,31 +241,20 @@ if __name__ == "__main__":
 
     # on initialise le journal avec le nom enless :
     logger = logging.getLogger('enless')
-    """
-    on prévoit 2 cas :
-    - journalisation dans un fichier
-    - journalisation à l'écran
-    """
     if args.log:
-        print(args.log)
+        # journalisation dans un fichier
         import logging.handlers
         loghandler = logging.handlers.RotatingFileHandler(args.log, maxBytes=5000*1024, backupCount=1)
     else:
+        # journalisation à l'écran
         loghandler = logging.StreamHandler()
     loghandler.setLevel("DEBUG")
     logger.addHandler(loghandler)
     loghandler.setFormatter(logging.Formatter("%(asctime) s %(levelname) 8s - %(message) s"))
-    """
-    initialisation du fichier conf
-    si conf vaut None, aucune mesure ne sera envoyée vers emoncms
-    """
-    conf = None
-    try:
-        with open(args.conf) as f:
-            conf =  json.loads(f.read())
-    except Exception as e:
-        logger.error("fichier de configuration mal formé %s", e)
-
-    sniffer = DataFlow(conf)
-    sniffer.run(url)
-    sniffer.close()
+    
+    if args.conf:
+        enless = DataFlow(args.conf)
+    else:
+        enless = DataFlow("ota2tcp.conf")
+    enless.run(url, port)
+    enless.close()
