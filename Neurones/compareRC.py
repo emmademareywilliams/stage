7e17a711bb02adc5bb09c8611508b9f0d724b889
emmademareywilliams <emma.demarey-williams@grenoble-inp.org>
2021-09-08 15:29:24 +0200

    identification des familles de (R, C)

--- Neurones/compareRC.py
+++ Neurones/compareRC.py
@@ -27,7 +27,7 @@ npoints =  length // interval
 Text = PyFina(circuit["Text"], dir, _tss, interval, npoints)
 
 # valeur initiale de température pour la convolution :
-T0 = 20
+T0 = 15
 
 # dictionnaire des différentes valeurs (R,C) :
 # Rvalues[0] --> circuit cellule
@@ -37,36 +37,57 @@ T0 = 20
 Rvalues = [2.59e-4, 2.95e-4, 3.30e-4, 5.20e-4, 1.06e-3, 9.85e-4, 1.89e-4, 1.14e-3]
 Cvalues = [1.31e9, 1.89e9, 1.53e9, 1.31e9, 2.05e9, 2.19e9, 2.61e8, 4.14e8]
 
+# on distingue 3 grandes familles de couples (R,C) au niveau du comportement sans chauffage :
+Rfamily = [2e-4, 3e-4, 1e-3]
+Cfamily = [2e8, 2e9, 2e9]
+
 
 message = "quel mode ? \n 1 --> chauffage constant ; 2 --> pas de chauffage \n"
 mode = input(message)
 if mode == "1":
-    Qc = np.ones(wsize)
+    Qc = np.ones(wsize)*max_power
 else:
-    Qc = np.zeros(wsize)
+    Qc = np.zeros(wsize)*max_power
 
 while True:
-    ts = getRandomStart(_tss,_tse, 10, 5)
+    ts = getRandomStart(_tss,_tse, 10, 5) # comportement en hiver
+    #ts = getRandomStart(_tss, _tse, 4, 8) # comportement en été
     pos = (ts - _tss) // interval
     Text_ep = Text[pos:pos+wsize]
+    """
     RCdata = []
     for i in range(len(Rvalues)):
         R = Rvalues[i]
         C = Cvalues[i]
         convo = R1C1sim(interval, R, C, Qc, Text_ep, T0)
         RCdata.append(convo)
+    """
+
+    RCfamily = []
+    for i in range(len(Rfamily)):
+        R = Rfamily[i]
+        C = Cfamily[i]
+        convo = R1C1sim(interval, R, C, Qc, Text_ep, T0)
+        RCfamily.append(convo)
 
     title = "Timestamp {} / {}".format(ts, tsToHuman(ts))
 
     xr = np.arange(ts, ts+wsize*interval, interval)
+
+    """
     for i in range(len(Rvalues)):
         if i==0:
-            plt.plot(RCdata[i], label="Cellule : R : {0:.2e} // C : {0:.2e}".format(Rvalues[i], Cvalues[i]))
-        if i in range(1,4):
-            plt.plot(RCdata[i], 'o', label="Nord : R : {0:.2e} // C : {0:.2e}".format(Rvalues[i], Cvalues[i]))
-        if i in range(5,7):
-            plt.plot(RCdata[i], '--', label="Sud : R : {0:.2e} // C : {0:.2e}".format(Rvalues[i], Cvalues[i]))
-    plt.plot(Text_ep, '--', label="température extérieure")
+            plt.plot(RCdata[i], ':', label="Cellule : R : {:.2e} // C : {:.2e}".format(Rvalues[i], Cvalues[i]))
+        if i in range(1,5):
+            plt.plot(RCdata[i], label="Nord : R : {:.2e} // C : {:.2e}".format(Rvalues[i], Cvalues[i]))
+        if i in range(5,8):
+            plt.plot(RCdata[i], '--', label="Sud : R : {:.2e} // C : {:.2e}".format(Rvalues[i], Cvalues[i]))
+    """
+
+    for i in range(len(Rfamily)):
+        plt.plot(RCfamily[i], '--', linewidth=2, label="famille {}".format(i+1))
+
+    plt.plot(Text_ep, linewidth=3, label="température extérieure")
 
     plt.legend(loc='lower left', ncol=2)
     plt.title(title)
